4179a4b53772770ea4d78c3030c86b7b33040df3
yamenbh <aymaneaitelbhiri14@gmail.com>
2023-10-19 15:34:47 +0100

    seconde commit

--- src/client/Machine.java
+++ src/client/Machine.java
@@ -1,5 +1,13 @@
 package client;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import service.IMachineService;
+
 public class Machine {
 
 	private int id;
@@ -46,5 +54,29 @@ public class Machine {
 	public void setPrix(double prix) {
 		this.prix = prix;
 	}
+	
+	public static void main(String[] args) {
+        try {
+            IMachineService machineService = (IMachineService) Naming.lookup("rmi://10.10.20.150:1099/imachineService");
+
+
+            Machine newMachine = new Machine(1, "123", "msi", 1000.0);
+            machineService.ajouterMachine(newMachine);
+            System.out.println("Machine ajoutée avec succès.");
+
+
+            String refMachineToDelete = "123";
+            machineService.supprimerMachine(refMachineToDelete);
+            System.out.println("Machine supprimée avec succès.");
+
+
+            List<Machine> machines = machineService.listerMachines();
+            for (Machine machine : machines) {
+                System.out.println("ID: " + machine.getId() + ", Référence: " + machine.getRef() + ", Marque: " + machine.getMarque() + ", Prix: " + machine.getPrix());
+            }
+        } catch (MalformedURLException | RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/client/Salle.java
+++ src/client/Salle.java
@@ -1,5 +1,13 @@
 package client;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import service.ISalleService;
+
 public class Salle {
 	
 	private int id;
@@ -26,6 +34,26 @@ public class Salle {
 	public void setCode(String code) {
 		this.code = code;
 	}
+	public static void main(String[] args) {
+        try {
+            ISalleService salleService = (ISalleService) Naming.lookup("rmi://10.10.20.150:1099/isalleService");
+
+            Salle newSalle = new Salle(1, "Salle123");
+            salleService.ajouterSalle(newSalle);
+            System.out.println("Salle ajoutée avec succès.");
+
+            String nomSalleToDelete = "Salle123";
+            salleService.supprimerSalle(nomSalleToDelete);
+            System.out.println("Salle supprimée avec succès.");
+
+            List<Salle> salles = salleService.listerSalles();
+            for (Salle salle : salles) {
+                System.out.println("ID: " + salle.getId() + ", Code: " + salle.getCode());
+            }
+        } catch (MalformedURLException | RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	
 	
